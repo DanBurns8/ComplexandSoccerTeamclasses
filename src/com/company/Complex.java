26c950cb61767b31a5038fe313d34e84a03f716a
MathEx <danielburns@gmail.com>
2022-01-25 21:04:23 -0500

    Daniel Burns

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -1,5 +1,61 @@
 package com.company;
 
-public class Complex {
+import java.text.CompactNumberFormat;
 
+public class Complex {
+    private double x;
+    private double y;
+
+    public Complex(double r, double i){
+        x=r;
+        y=i;
+    }
+    public Complex(double r){
+        this(r,0.0);
+    }
+    public String toString(){
+        String s=x+"+"+y+"i";
+        return s;
+    }
+    public double abs(){
+        double a=Math.pow(x,2);
+        double b=Math.pow(y,2);
+        return Math.sqrt(a+b);
+    }
+    public Complex add(Complex other){
+        double a=x+other.x;
+        double b=y+other.y;
+        Complex c= new Complex(a,b);
+        return c;
+    }
+    public Complex add(double s){
+        double a=x+s;
+        double b=y;
+        Complex c= new Complex(a,b);
+        return c;
+    }
+    public Complex multiply(Complex other){
+        double a=x*other.x-y+other.y;
+        double b=x*other.y+y*other.x;
+        Complex c= new Complex(a,b);
+        return c;
+    }
+    //multiply(double s)
+    public static void main(String[] args) {
+        Complex s= new Complex(3.0,4.0);
+        Complex t= new Complex(5.0,8.0);
+        Complex c1= new Complex(7.0,9.0);
+        Complex c2= new Complex(-9.0,8.0);
+        Complex c3= new Complex(12.0,-8.0);
+        Complex c4= new Complex(13.0,17.0);
+        System.out.println(s.abs());
+        System.out.println(t.abs());
+        System.out.println(s.multiply(t));
+        System.out.println(c1.multiply(c2));
+        System.out.println(c3.multiply(c4));
+        System.out.println(c3.add(c4));
+        System.out.println(t.add(s));
+        System.out.println(c2.add(c1));
+        System.out.println(c4.add(2.0));
+    }
 }
--- src/com/company/SoccerTeam.java
+++ src/com/company/SoccerTeam.java
@@ -1,7 +1,91 @@
 package com.company;
 
 public class SoccerTeam {
-    public void played(team1, teamTwo, int myScore, int otherScore){
+    private int tie;
+    private int lose;
+    private int win;
+    private static int numGoals;
+    private static int numGames;
+    public SoccerTeam(){
+        tie=0;
+        lose=0;
+        win=0;
+        numGoals=0;
+        numGames=0;
+    }
+    public void played(SoccerTeam other, int myScore, int otherScore){
+        numGames++;
+        numGoals=numGoals+myScore+otherScore;
+        if(myScore>otherScore){
+            win++;
+            other.lose++;
+        }
+        if(myScore<otherScore){
+            lose++;
+            other.win++;
+        }
+        if(myScore==otherScore){
+            tie++;
+            other.tie++;
+        }
+    }
+    public int getPoints(){
+        return win*3+tie;
+    }
+    public void reset(){
+        tie=0;
+        lose=0;
+        win=0;
+    }
+    public static int getNumGames(){
+        return numGames;
+    }
+    public static int getNumGoals(){
+        return numGoals;
+    }
+//    public static int startTournament(){
+//
+//    }
 
+    public static void main(String[] args) {
+        SoccerTeam tiger = new SoccerTeam();
+        SoccerTeam dolphin = new SoccerTeam();
+        SoccerTeam pats = new SoccerTeam();
+        SoccerTeam bucaneers = new SoccerTeam();
+
+        startTournament();
+        System.out.println("Tournament 1");
+        tiger.played(dolphin,6,7);
+        dolphin.played(pats,6,6);
+        pats.played(bucaneers,9,10);
+        bucaneers.played(dolphin,5,4);
+
+        System.out.println("Tigers point:"+tiger.getPoints());
+        System.out.println("Pats point:"+pats.getPoints());
+        System.out.println("Dolphin point:"+dolphin.getPoints());
+        System.out.println("Bucaneers point:"+bucaneers.getPoints());
+        System.out.println("Total Games:"+getNumGames());
+        System.out.println("Total Goals:"+getNumGoals());
+
+        dolphin.reset();
+        tiger.reset();
+        bucaneers.reset();
+        pats.reset();
+
+        startTournament();
+
+        System.out.println("Tournament 2");
+
+        tiger.played(dolphin,8,7);
+        dolphin.played(pats,6,12);
+        pats.played(bucaneers,7,10);
+        bucaneers.played(dolphin,5,19);
+
+        System.out.println("Tigers point:"+tiger.getPoints());
+        System.out.println("Pats point:"+pats.getPoints());
+        System.out.println("Dolphin point:"+dolphin.getPoints());
+        System.out.println("Bucaneers point:"+bucaneers.getPoints());
+        System.out.println("Total Games:"+getNumGames());
+        System.out.println("Total Goals:"+getNumGoals());
     }
 }
